d8cdee273de5441efb18b27c54e13e7843851d85
tylerlidster <tyler.lidster@me.com>
2016-04-15 16:19:23 -0700

    Created Login Page
    Create html and use bootstrap to create login page and style

---
+++ SHIFT/application/views/pages/login.php
@@ -0,0 +1,44 @@
+<div class="container-fluid">
+
+	<div class="row">
+		<div class="col-md-12">
+			<img class="img-responsive" src="images/SHIFT_header.png" alt="SHIFT">
+		<div>
+	</div>
+
+	<div class="row">
+  		<div class="col-md-4"></div>
+  		<div class="col-md-4">
+  		<br><br><br><br>
+  	
+  	<form role="form">
+  		<div class="col-md-10">
+  		<div class="form-group">
+  			<input type="text" class="form-control input-lg" id="name" placeholder="NAME" required>
+  		</div>
+
+  		<div class="form-group">
+  			<input type="password" class="form-control input-lg" id="password" placeholder="PASSWORD" required>
+  		</div>
+
+  		<div class="form-group">
+  		<input type="email" class="form-control input-lg" id="email" placeholder="EMAIL" required>
+  		</div>
+	
+		<div class="form-group">
+  		<input type="number" class="form-control input-lg" id="phone" placeholder="PHONE" required>
+  		</div>
+		
+  		<button type="submit" class="btn btn-primary btn-block btn-lg">SUBMIT</button>
+		
+		</div>
+	</form>
+
+  		</div>
+
+  		<div class="col-md-4"></div>
+
+	</div>
+	
+
+</div>
